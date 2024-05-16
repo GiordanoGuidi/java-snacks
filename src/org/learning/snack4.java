43c34dfb119c3c6b4f9b886b708e1b13f969e558
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-05-16 15:41:59 +0200

    solved snack4

---
+++ src/org/learning/snack4.java
@@ -0,0 +1,23 @@
+package org.learning;
+
+import java.util.Scanner;
+
+public class snack4 {
+    public static void main(String[] args) {
+        /*Snack4
+        Data in input una stringa verificare se è palindroma*/
+        Scanner scanner=new Scanner(System.in);
+        //Chiedo all'utente una parola
+        System.out.print("Dimmi una parola");
+        //Assegno il valore alla variabile word
+        String word =(scanner.nextLine());
+        //Capovolgo i caratteri della stringa
+        StringBuilder reverseWord= new StringBuilder(word).reverse();
+        //Controllo se la parola di partenza è uguale a quella capovolta
+        if (word.equals(reverseWord.toString())){
+            System.out.println("La parola è palindroma");
+        }else {
+            System.out.println("La parola non  è palindroma");
+        }
+    }
+}
